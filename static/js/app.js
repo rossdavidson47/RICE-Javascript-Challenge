a0c3a321fceff3cd2399481b2210d3ac887fa241
rossdavidson47 <rossatwarwick@hotmail.com>
2020-08-16 22:43:14 -0500

    level 2

--- static/js/app.js
+++ static/js/app.js
@@ -3,16 +3,12 @@ var tableData = data;
 
 // Grab the table and ids
 const tableBody = d3.select('tbody');
-// Select the filter button
-const filterButton = d3.select('#filterButton');
 // Select the clear filter button
 const clearButton = d3.select('#clearButton');
-// Select the form
-const dateForm = d3.select('#form');
-// Select the date
-const dateValue = d3.select('#datetime');
+// Select the filter button
+const filterButton = d3.select('#filterButton');
 
-//Populate table from data file (uses code from 03-Evr_D3_Table
+//Populate table from data file (uses code from 03-Evr_D3_Table)
 function buildTable(dataSample){
     dataSample.forEach((dataReport) => {
         var row = tableBody.append("tr");
@@ -27,29 +23,57 @@ function buildTable(dataSample){
 buildTable(data);
 
 // Create event handlers for clicking the button or pressing the enter key
-filterButton.on("click", runFilterFunction);
-dateForm.on("submit", runFilterFunction);
+// dateForm.on("submit", runFilterFunction);
 clearButton.on("click", runClearFunction);
+filterButton.on("click", runFilterFunction);
 
-// Create the function to run for both events
-function runFilterFunction(){
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  // Get the value property of the input element
-  var inputValue = dateValue.property("value");
-  //print to console to check
-  console.log(inputValue);
-  //find filtered data
-  var filteredData = tableData.filter(row => row.datetime === inputValue);
-  //print to console to check
-  console.log(filteredData);
-  //clear table
-  tableBody.selectAll('tr').remove()
-  buildTable(filteredData)
-}
-
+//Clear filters and rebuild
 function runClearFunction(){
     tableBody.selectAll('tr').remove()
     buildTable(data);
+//Please tell me how to reset the form defaults to blank?? I tried so hard
 }
 
+// Create the function to run filters
+function runFilterFunction(){
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  //initialise filterData with full data
+  filteredData = data
+
+  //find filtered data by date 
+  var inputValue = d3.select('#datetime').property("value");
+    if (inputValue != "") {
+        console.log(inputValue);
+        var filteredData = filteredData.filter(row => row.datetime === inputValue);
+        }
+  //find filtered data by city 
+  var inputValue = d3.select('#city').property("value");
+    if (inputValue != "") {
+        console.log(inputValue);
+        var filteredData = filteredData.filter(row => row.city === inputValue);
+        }
+  //find filtered data by State 
+  var inputValue = d3.select('#state').property("value");
+    if (inputValue != "") {
+        console.log(inputValue);
+        var filteredData = filteredData.filter(row => row.state === inputValue);
+        }
+  //find filtered data by Country 
+  var inputValue = d3.select('#country').property("value");
+    if (inputValue != "") {
+        console.log(inputValue);
+        var filteredData = filteredData.filter(row => row.country === inputValue);
+        }
+  //find filtered data by Shape
+  var inputValue = d3.select('#shape').property("value");
+    if (inputValue != "") {
+        console.log(inputValue);
+        var filteredData = filteredData.filter(row => row.shape === inputValue);
+        }
+  //print to console to check
+  console.log(filteredData);
+  //clear table and rebuild with filtered data
+  tableBody.selectAll('tr').remove()
+  buildTable(filteredData)
+}
